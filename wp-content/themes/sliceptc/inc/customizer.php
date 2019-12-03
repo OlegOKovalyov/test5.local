9db1bcd9b495aae844ca2283832ac33627160319
OlegOKovalyov <koa2003@ukr.net>
2019-12-03 21:33:39 +0200

    Initial commit

---
+++ wp-content/themes/sliceptc/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Sliceptc Theme Customizer
+ *
+ * @package Sliceptc
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function sliceptc_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'sliceptc_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'sliceptc_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'sliceptc_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function sliceptc_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function sliceptc_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function sliceptc_customize_preview_js() {
+	wp_enqueue_script( 'sliceptc-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'sliceptc_customize_preview_js' );
